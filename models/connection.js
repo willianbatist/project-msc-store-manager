fafe2bb39d3454fb769c4c76b0b2d33f2da6adfd
Willian <willian.alves.b15@gmail.com>
2022-05-06 16:54:39 -0300

    feat: added connection

--- models/connection.js
+++ models/connection.js
@@ -7,6 +7,7 @@ const connection = mysql.createPool({
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE || 'StoreManager',
+  port: process.env.PORT,
 });
 
 module.exports = connection;
