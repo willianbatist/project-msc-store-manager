e87ac8bae77d141b387fdff973e05620b943f679
Willian <willian.alves.b15@gmail.com>
2022-05-06 17:59:04 -0300

    feat: create dir routes and import router in index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const app = require('./app');
 require('dotenv').config();
-
+const router = require('./routes/index');
 // não altere esse arquivo, essa estrutura é necessária para à avaliação do projeto
 
+app.use(router);
 app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
 });
