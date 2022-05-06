2367afd400b544451f547b2aeb9be8ba13272ea4
Willian <willian.alves.b15@gmail.com>
2022-05-06 18:52:26 -0300

    feat: router /products

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,6 @@
 const router = require('express').Router();
+const productsController = require('../controllers/productsController');
+
+router.get('/products', productsController.getAllControllerProducts);
 
 module.exports = router;
