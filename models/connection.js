1d6ce15619f510ba447c7037c8b64a0cbaad35f1
Willian <willian.alves.b15@gmail.com>
2022-05-06 17:26:55 -0300

    fix: connection port

--- models/connection.js
+++ models/connection.js
@@ -7,7 +7,7 @@ const connection = mysql.createPool({
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE || 'StoreManager',
-  port: process.env.PORT,
+  port: process.env.MYSQL_PORT,
 });
 
 module.exports = connection;
