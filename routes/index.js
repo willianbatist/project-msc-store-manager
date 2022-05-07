706fac07129576c0259e8ed99cc752da84501d38
Willian <willian.alves.b15@gmail.com>
2022-05-07 15:52:11 -0300

    feat: added in router getAllControllerSales

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 const productsController = require('../controllers/productsController');
+const salesController = require('../controllers/salesController');
 
 router.get('/products', productsController.getAllControllerProducts);
 router.get('/products/:id', productsController.getProductId);
+
+router.get('/sales', salesController.getAllControllerSales);
 
 module.exports = router;
