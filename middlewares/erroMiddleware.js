b42447678b5cc92ccccccd3ae41b59bf0e2694f0
Willian <willian.alves.b15@gmail.com>
2022-05-06 21:42:14 -0300

    feat: added erroHandle

---
+++ middlewares/erroMiddleware.js
@@ -0,0 +1,8 @@
+const erroHandler = (status, message) => ({
+    status,
+    message,
+  });
+
+module.exports = {
+  erroHandler,
+};
---
+++ middlewares/validateProducts.js
@@ -0,0 +1,14 @@
+// // Middleware
+// const Joi = require('joi');
+
+// const ID = Joi.object({
+//   id: Joi.required(),
+// });
+
+// const validateProductId = (id) => {
+  
+// };
+
+// module.exports = {
+//   validateProductId,
+// };
