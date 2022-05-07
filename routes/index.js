473875455ac0d63b1e20a52a308c8c55653e7053
Willian <willian.alves.b15@gmail.com>
2022-05-06 21:48:09 -0300

    feat: added router getProductId

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const productsController = require('../controllers/productsController');
 
 router.get('/products', productsController.getAllControllerProducts);
+router.get('/products/:id', productsController.getProductId);
 
 module.exports = router;
