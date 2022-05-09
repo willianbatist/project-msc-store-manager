d899249784aa497cc003e7149f0c658adcd9f9bc
Willian <willian.alves.b15@gmail.com>
2022-05-09 16:21:43 -0300

    feat: validateQuantity

--- middlewares/validateProducts.js
+++ middlewares/validateProducts.js
@@ -18,10 +18,10 @@ const validateName = async (req, res, next) => {
 const validateQuantity = async (req, res, next) => {
   try {
     const { quantity } = req.body;
-    if (!quantity) {
+    if (quantity === undefined) {
       return res.status(400).json({ message: '"quantity" is required' });
     }
-    if (quantity >= 0) {
+    if (quantity <= 0) {
       return res.status(422).json({ message: '"quantity" must be greater than or equal to 1' });
     }
     return next();
