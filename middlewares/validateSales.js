39605a6e312afea352546932f0ad6916f2148a53
Willian <willian.alves.b15@gmail.com>
2022-05-10 15:34:27 -0300

    fix: remove commet

--- middlewares/validateSales.js
+++ middlewares/validateSales.js
@@ -4,12 +4,6 @@ const validateProduct = (req, res, next) => {
     if (productKey) {
       return res.status(400).json({ message: '"productId" is required' });
     }
-    // const validate = (r) => {
-      // if (r.productId === undefined) {
-      //   return res.status(400).json({ message: '"productId" is required' });
-      // }
-    // };
-    // req.body.map((r) => validate(r));
     return next();
   } catch (error) {
     return res.status(500).send({ error: error.message });
@@ -26,15 +20,6 @@ const validateQuantity = (req, res, next) => {
     if (missingValue) {
       return res.status(400).json({ message: '"quantity" is required' });
     }
-    // const validate = (r) => {
-    //   if (r.quantity === undefined) {
-    //     return res.status(400).json({ message: '"quantity" is required' });
-    //   }
-    //   if (r.quantity <= 0) {
-    //     return res.status(422).json({ message: '"quantity" must be greater than or equal to 1' });
-    //   }
-    // };
-    // req.body.map((r) => validate(r));
     return next();
   } catch (error) {
     return res.status(500).send({ error: error.message });
