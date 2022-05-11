be4225944681f50afe9b5e94bae57a44c81ffa17
Willian <willian.alves.b15@gmail.com>
2022-05-10 22:24:31 -0300

    test: created two first test

---
+++ test/unit/models/productsModels.js
@@ -0,0 +1,50 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const connection = require('../../../models/connection');
+const productsModel = require('../../../models/productsModel');
+
+describe('Camada Model, Testando  productsServices', () => {
+  describe('quando existem Produtos registrado no BD', () => {
+    // it('', () =>{})
+    // estrtura do BD [[{ id }]]
+    const resultExecute = [
+      {
+        "id": 1,
+        "name": "Martelo de Thor",
+        "quantity": 10
+      },
+      {
+        "id": 2,
+        "name": "Traje de encolhimento",
+        "quantity": 20
+      },
+      {
+        "id": 3,
+        "name": "Escudo do Capitão América",
+        "quantity": 30
+      }
+    ];
+
+    before(() => {
+      sinon.stub(connection, 'execute')
+        .resolves(resultExecute)
+    })
+    after(() => {
+      connection.execute.restore();
+    })
+
+    it('Array com os produtos', async () =>{
+      const result = await productsModel.getAllModelProducts();
+      expect(result).to.be.not.empty;
+    })
+
+    it('o objeto que esta no array contem os atributos id, name, quantity', async () => {
+      const result = await productsModel.getAllModelProducts();
+      expect(result).to.be.includes.all.keys(
+        'id',
+        'name',
+        'quantity'
+      )
+    })
+  });
+});
