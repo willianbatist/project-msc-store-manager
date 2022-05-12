f92f18d8cd797591f0cb34afd1ab328f4559fda8
Willian <willian.alves.b15@gmail.com>
2022-05-12 17:47:16 -0300

    test: created new coverage getSalesId model

--- test/unit/models/salesModels.js
+++ test/unit/models/salesModels.js
@@ -33,4 +33,20 @@ describe('Camada Models, SalesModels', () => {
 
     connection.execute.restore();
   });
+
+  it('a venda buscada no BD apresenta saleId, date, productId e quantity do produto', async () =>{
+    const resultExecute = [{
+      "saleId": 1,
+      "date": "2022-05-12T23:05:53.000Z",
+      "productId": 1,
+      "quantity": 5
+    }];
+    sinon.stub(connection, 'execute')
+    .resolves(resultExecute)
+
+    const result = await salesModel.getSalesId(1);
+
+    expect(result.saleId).to.be.equal(1)
+  connection.execute.restore();
+  })
 });
